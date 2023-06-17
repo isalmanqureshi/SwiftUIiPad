2a39578b5f2781bb1ae4b659bc18dfd04aff600b
Salman Qureshi <er.salmanqureshi@gmail.com>
2023-06-17 20:11:04 +0400

    Created a new `SwiftUI` project

---
+++ SwiftUIiPad/SwiftUIiPad/SwiftUIiPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiPadApp.swift
+//  SwiftUIiPad
+//
+//  Created by Salman Qureshi on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
