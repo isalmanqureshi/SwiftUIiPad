823a6279c032a7bd9bbdb2950f43d6aa4ff36ed8
Salman Qureshi <er.salmanqureshi@gmail.com>
2023-06-18 01:57:03 +0400

    Added logo, implemented collection view type cell

---
+++ SwiftUIiPad/SwiftUIiPad/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  SwiftUIiPad
+//
+//  Created by Salman Qureshi on 6/18/23.
+//
+
+import SwiftUI
+
+struct CollectionViewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CollectionViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        CollectionViewCell()
+    }
+}
